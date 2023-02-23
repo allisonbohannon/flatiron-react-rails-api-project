9f229a88f095c01103517491f51930880760a537
Allison Bohannon <akboh.github@gmail.com>
2023-02-23 10:53:16 -0800

    Update comment car for correct nesting

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -9,9 +9,9 @@ const CommentCard = ({comment, wineries, users}) => {
 
     const {currentUser} = useContext(UserContext)
 
-    const checkIfCurrentUser = currentUser.id === comment.userId ? true : false 
-    const targetUser = users.find(user => comment.userId = user.id)
-    const targetWinery = wineries.filter(winery => comment.wineryId === winery.id)
+    const checkIfCurrentUser = currentUser.id === comment.user.id ? true : false 
+    const targetUser = users.find(user => comment.user.id = user.id)
+    const targetWinery = wineries.filter(winery => comment.winery.id === winery.id)
     
     const author = <p style={{fontStyle:"italic"}}>{targetUser.username}</p>
     const button = <Link to={`/wineries/${targetWinery.id}/comments/${comment.id}/edit`}>
